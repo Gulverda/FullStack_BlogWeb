70b96aeeab62fd09e181ed27ec626caa2d18b73b
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-12-01 17:59:29 +0400

    Added Project

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+// Admin login
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router;
---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./configuration/dbConfig.js";
+import blogRoutes from "./routes/blogRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+app.use(cors());
+
+// Database Connection
+connectDB();
+
+// Routes
+app.use("/api/blogs", blogRoutes);
+app.use("/api/auth", authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Blog API');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
